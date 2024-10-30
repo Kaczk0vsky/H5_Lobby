f4854d53360e07c899335212a91711e617020014
Kaczk0vsky <piterspp1998@gmail.com>
2024-10-30 14:07:26 +0100

    fix: changed naming

--- src/settings_reader.py
+++ src/settings_reader.py
@@ -3,4 +3,4 @@ import toml
 
 
 def load_initial_window_setting():
-    return toml.load(os.path.join(os.getcwd(), "settings.toml"))["initial_window"]
+    return toml.load(os.path.join(os.getcwd(), "settings.toml"))["resolution"]
