c0e6262ed8b2650ce264c445010ee81fa6c0ca1d
Kaczk0vsky <piterspp1998@gmail.com>
2024-11-06 16:05:50 +0100

    fix: fixed the structure

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from login import LoginWindow
+from src.login import LoginWindow
 
 
 def run_lobby():
     """Run the H5_Lobby GUI."""
     lobby = LoginWindow()
     lobby.run_game()
+
+
+if __name__ == "__main__":
+    run_lobby()
