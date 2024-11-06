e9ae5534519c395f8aade34ffd129ce144e6cbb2
Kaczk0vsky <piterspp1998@gmail.com>
2024-11-06 16:28:56 +0100

    fix: fixed urls again

--- admin_settings/urls.py
+++ admin_settings/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
+from h5_backend import views
 
 urlpatterns = [
     path("h5_backend/", include("h5_backend.urls")),
