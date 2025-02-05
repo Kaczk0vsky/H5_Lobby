64e28f3d04bea69da6ed1eeada4ad2066d3f7ff2
Kaczk0vsky <piterspp1998@gmail.com>
2025-02-05 23:36:21 +0100

    fix: added loop if not corectly selected

--- src/settings_reader.py
+++ src/settings_reader.py
@@ -19,21 +19,30 @@ def check_for_missing_paths():
     is_saved = False
     with open(os.path.join(os.getcwd(), "settings.toml"), "r") as f:
         data = toml.load(f)
-        if data["game"]["game_path"] == "":
-            data["game"]["game_path"] == easygui.fileopenbox(
-                msg="Plese select the path to AshanArena3.exe",
-                title="AshanArena3",
-                default=".exe",
-            )
-            is_saved = True
-
-        if data["soft_ether"]["vpn_path"] == "":
-            data["soft_ether"]["vpn_path"] = easygui.fileopenbox(
-                msg="Plese select the path to SoftEtherVPN.exe",
-                title="SoftEtherVPN",
-                default=".exe",
-            )
-            is_saved = True
+        while True:
+            path_str = str(data["game"]["game_path"]).lower()
+            if "AshanArena3".lower() not in path_str:
+                if data["game"]["game_path"] == "":
+                    data["game"]["game_path"] == easygui.fileopenbox(
+                        msg="Plese select the path to AshanArena3.exe",
+                        title="AshanArena3",
+                        default=".exe",
+                    )
+                    is_saved = True
+            if "AshanArena3".lower() in path_str:
+                    break
+        
+        while True:
+            path_str = str(data["soft_ether"]["vpn_path"]).lower()
+            if "SoftEther".lower() not in path_str:
+                data["soft_ether"]["vpn_path"] = easygui.fileopenbox(
+                    msg="Plese select the path to SoftEtherVPN.exe",
+                    title="SoftEtherVPN",
+                    default=".exe",
+                )
+                is_saved = True
+            if "SoftEther".lower() in path_str:
+                break
 
     if is_saved:
         with open(os.path.join(os.getcwd(), "settings.toml"), "w") as f:
