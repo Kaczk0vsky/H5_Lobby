a459b42eff443f6ac91da677adb4649b766aed03
Kaczk0vsky <piterspp1998@gmail.com>
2025-03-10 19:00:52 +0100

    feat: added delete player with user deletetion

--- h5_backend/signals.py
+++ h5_backend/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from h5_backend.models import Player
@@ -9,3 +9,8 @@ def create_player(sender, instance, created, **kwargs):
     if created:
         # Automatically create a Player instance when a new User is created
         Player.objects.create(nickname=instance.username)
+
+
+@receiver(post_delete, sender=User)
+def delete_player(sender, instance, **kwargs):
+    Player.objects.filter(nickname=instance.username).delete()
