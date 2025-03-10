fb8132684568e5a92e3de71e237c2b1e9e0187e1
Kaczk0vsky <piterspp1998@gmail.com>
2025-03-10 18:33:37 +0100

    fix 2

--- h5_backend/tasks.py
+++ h5_backend/tasks.py
@@ -6,9 +6,10 @@ import subprocess
 def add_new_user_to_vpn_server(
     vpn_server_ip: str, vpn_admin_password: str, vpncmd_commands: str
 ) -> bool:
+    vpncmd_path = "/usr/local/vpnserver/vpncmd"
     try:
         command = [
-            "vpncmd",
+            vpncmd_path,
             vpn_server_ip,
             "/SERVER",
             f"/PASSWORD:{vpn_admin_password}",
