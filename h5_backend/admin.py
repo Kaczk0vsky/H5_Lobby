93a240ae45e853533c9ac630d32b953045f2a15a
Kaczk0vsky <piterspp1998@gmail.com>
2025-03-13 10:48:33 +0100

    fix: registered model in admin

--- h5_backend/admin.py
+++ h5_backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Player, Game
+from .models import Player, Game, PlayersMatched
 
 admin.site.register(Player)
 admin.site.register(Game)
+admin.site.register(PlayersMatched)
