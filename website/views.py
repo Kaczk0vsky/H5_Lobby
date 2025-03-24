4a2cce86ce9bdf42c51ea32058f25b8dd422d389
Kaczk0vsky <piterspp1998@gmail.com>
2025-03-24 23:19:13 +0100

    feat: added custom view for website

--- website/views.py
+++ website/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def ashanarena(request):
-    return HttpResponse(
-        "Greetings, Noble Warrior! Behold, the AshanArena3 is currently under construction. Take heed and return in the future, for great wonders shall await thee... Thou shalt not be disappointed!"
-    )
+    return render(request, "main.html")
