ffc05a4ddf5945b7055f55f24e6b18d737b0907a
Kaczk0vsky <piterspp1998@gmail.com>
2025-03-30 23:26:27 +0200

    chore: added transaction safety, link is no longer usefull after using

--- website/views.py
+++ website/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.forms import SetPasswordForm
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 from django_ratelimit.decorators import ratelimit
+from django.db import transaction
+from django.contrib.sessions.models import Session
 
 
 @csrf_protect
@@ -26,9 +28,13 @@ def password_reset_confirm(request, uidb64, token):
         form = SetPasswordForm(user, request.POST)
         if form.is_valid():
             new_password = form.cleaned_data["new_password1"]
-            user.set_password(new_password)
-            user.save()
-            return render(request, "password_reset_complete.html")
+            with transaction.atomic():
+                user.refresh_from_db()
+                user.set_password(new_password)
+                user.last_login = None
+                user.save()
+                Session.objects.filter(session_key__in=request.session.keys()).delete()
+                return render(request, "password_reset_complete.html")
     else:
         form = SetPasswordForm(user)
 
