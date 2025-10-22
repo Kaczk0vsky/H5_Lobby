d65a018802bc5923fcd330592d9c1026acabf203
Kaczk0vsky <piterspp1998@gmail.com>
2025-10-22 11:42:26 +0200

    feat: added admin searching fields

--- h5_backend/admin.py
+++ h5_backend/admin.py
@@ -1,7 +1,48 @@
 from django.contrib import admin
+from django.db.models import Q
 
-from .models import Player, Game, Ban
+from h5_backend.models import Player, Game, Ban
 
-admin.site.register(Player)
-admin.site.register(Game)
-admin.site.register(Ban)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("id", "nickname", "ranking_points", "ranking_position", "player_state")
+    list_filter = ("player_state",)
+    ordering = ("-ranking_points",)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "player_1",
+        "player_2",
+        "castle_1",
+        "castle_2",
+        "points_change_winner",
+        "points_change_loser",
+        "who_won",
+        "is_ranked",
+        "is_waiting_confirmation",
+        "is_different",
+    )
+
+    list_filter = (
+        "is_ranked",
+        "is_waiting_confirmation",
+        "is_different",
+    )
+
+    search_fields = (
+        "player_1__nickname",
+        "player_2__nickname",
+        "who_won__nickname",
+        "castle_1",
+        "castle_2",
+    )
+
+
+@admin.register(Ban)
+class BanAdmin(admin.ModelAdmin):
+    list_display = ("id", "player", "created_at", "duration")
+    list_filter = ("created_at",)
